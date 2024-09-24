f46837a3359dd0adddcd664ddbdf6c63b90ab06a
lexynotfound <raihanardila@gmail.com>
2024-09-24 21:49:38 +0700

    fixing

--- src/validators/collabValid.js
+++ src/validators/collabValid.js
@@ -1,6 +1,5 @@
 const Joi = require('joi');
 
-// Collaboration validation schema
 const collaborationSchema = Joi.object({
   playlistId: Joi.string().required(),
   userId: Joi.string().required(),
